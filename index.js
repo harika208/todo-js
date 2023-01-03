387705645820d052153678beb1c11a69481ee516
harika208 <harikahn119@gmail.com>
2023-01-03 22:32:20 +0530

    commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function addpop(){
     let popup = document.querySelector("#popup-main");
     popup.style.display = "flex";
-
+    
 }
 function add2pop(){
     let popup123 = document.querySelector("#popup-main1");
     popup123.style.display = "flex";
+    
 }
 
 
@@ -15,25 +16,41 @@ const taskList = [];
 const addTask = () => {
     const name = document.getElementById('name').value;
     console.log(name);
+
+    if (taskList.length === 0) {
+        const exstingElement12 = document.getElementsByClassName("no");
+        exstingElement12[0].innerText = "";
+    }
+     
     const tempObj = {
         id: Date.now(),
         taskName: name,
     }
+    
     taskList.push(tempObj);
     console.log(taskList);
-    addTaskOnScreen();
+    if(name.length == 0){
+        alert("Add content");
+        closepop();
+    }
+    else{
+        addTaskOnScreen();
+    }
     
+
 }
 
 
 
-const addTaskOnScreen = ()=>{
+    const addTaskOnScreen = ()=>{
+
+    
     const element = document.createElement("div");
-    element.setAttribute("class","child");
+    element.setAttribute("id","child");
 
     const existingElement = document.getElementById("parent");
     existingElement.appendChild(element);
-    element.innerText = taskList[taskList.length-1].taskName;
+    element.innerText =  taskList[taskList.length-1].taskName;
 
     let hori = document.createElement("hr");
     hori.setAttribute("width","100%");
@@ -41,39 +58,60 @@ const addTaskOnScreen = ()=>{
 
 
 
-    let plus1 = document.createElement("img");
-    plus1.setAttribute("src","./circle-plus-solid.svg");
-    element.appendChild(plus1).addEventListener("click",plus12);
-     function plus12(){
-        let popup12 = document.querySelector("#popup-main1")
-        popup12.style.display = "flex";
-        add2pop();
-     }
-    
-     
    
-    let delete1 = document.createElement("img");
+    
+     let delete1 = document.createElement("img");
     delete1.setAttribute("src","./trash-can-solid.svg");
     element.appendChild(delete1).addEventListener("click", () => {
         existingElement.removeChild(element);
     })
 
- 
+    let plus1 = document.createElement("img");
+    plus1.setAttribute("src","./circle-plus-solid.svg");
+    element.appendChild(plus1).addEventListener("click",add2pop);
 
+    let pop = document.querySelector('#popup-main');
+    pop.style.display = "none";
+
+const content = document.createElement("div");
+content.setAttribute("id","innerbox");
+content.style.display = "inline-block";
+element.appendChild(content);
+const btn = document.getElementById('btn');
+const name1 = document.getElementById("name1");
+
+btn.addEventListener('click', function(){
+    let text = document.createElement('p');
+    text.classList.add('paragraph-styling');
+    text.style.color = "black";
+    text.style.display = "inline-block";
+    content.appendChild(text);
+    text.innerText = name1.value;
+    name1.value = "";
+
+
+
+    let popup1 = document.querySelector("#popup-main1")
+            popup1.style.display = "none";
+
+
+    let mark = document.createElement('button');
+    mark.setAttribute('id','mark');
+    mark.innerHTML = "Mark - Done";
+    content.appendChild(mark);
+
+    mark.addEventListener('click',function(){
+        text.style.color = 'red';
+        text.style.textDecoration = "line-through";
+        mark.style.visibility = "hidden";
+    })
     
-    
-     let pop = document.querySelector('#popup-main');
-     pop.style.display = "none";
- 
-     let pup = document.querySelector('#popup-main1');
-     pup.style.display = "none";
+})
 
      
 
+     
 }
-
-  
-
 
 
 function closepop(){
@@ -85,7 +123,3 @@ function closepop(){
     white1.style.display = "none";
 
 }
-
-
-
-
